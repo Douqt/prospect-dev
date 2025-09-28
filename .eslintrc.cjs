040f11485f486fd6d74b018349a44753ae2302f1
DoubtfuII <galipavan06@gmail.com>
2025-09-28 16:38:32 -0500

    chore: eslint/next fixes + tailwind plugin import + ignore generated types

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+/**
+ * Minimal ESLint config to enable Next.js rules for projects using the flat config API
+ * This file uses the traditional config format to load the Next.js recommended rules.
+ */
+module.exports = {
+  extends: ["next/core-web-vitals"],
+};
