2d226414f4d92607452b36cfab6fef6eed912693
DoubtfuII <galipavan06@gmail.com>
2025-09-28 17:18:30 -0500

    chore: annotate files to document removal of internal 'lovable' tags

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+// Commit cleanup: removed internal lovable tags from earlier commits
+// This file was annotated as part of a non-destructive cleanup.
 export default {
   plugins: {
     tailwindcss: {},
